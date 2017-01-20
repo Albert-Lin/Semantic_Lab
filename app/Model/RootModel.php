80cb935fd88d97c8d72379345d6b26c4c76d38f5
Albert-Lin <familywithloveg@gmail.com>
2017-01-20 18:02:46 +0800

    Start using model to insert and select data

---
+++ app/Model/RootModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlbertLin
+ * Date: 2017/1/20
+ * Time: 上午 11:17
+ */
+
+namespace App\Model;
+
+
+class RootModel
+{
+
+}
---
+++ resources/views/test/css/grid.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlbertLin
+ * Date: 2017/1/20
+ * Time: 下午 01:11
+ */
