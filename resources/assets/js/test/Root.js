5eb70f914235b718fc2bcf24b6b97aed1a28a900
Albert-Lin <familywithloveg@gmail.com>
2017-03-21 23:28:12 +0800

    01. fix the error, now we can easily get the data from service and create root vue object

--- resources/assets/js/test/Root.js
+++ resources/assets/js/test/Root.js
@@ -4,82 +4,71 @@
 
 require('../require.js');
 
-// import templateLib from '../templates/TemplatesLib.js';
 
-let rootConfig = {
-	el: '#root',
-	components: undefined,
-	data: {
-		info: templates,
-	},
-	computed: {
-		currentTemplate: function(){
-			return this.info[0];
-		}
-	},
-	watch: {
-		info: function(newInfo){
-			rootConfig.components = require('../templates/TemplatesLib.js').default;
-			rootConfig.data.ajax = 'true';
-			window.ROOT = new Vue(rootConfig);
-		}
-	},
-	methods: {
-		init: function(){
-			console.log(rootConfig.components);
-			if(templates.length === 0){
-				axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-				axios({
-					url: 'http://semanticlab.com/test/vue/vue-ss',
-					method: 'post',
+(function(){
+	axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+	axios({
+		url: 'http://semanticlab.com/test/vue/vue-ss',
+		method: 'post',
+	})
+		.then(function(response){
+			let serviceData = response.data;
+			window.config
+				.setTemplate({
+				template: 'blank',
+				block0: {
+					componentData: [serviceData[0], serviceData[1], serviceData[2], serviceData[3], serviceData[4], serviceData[5]],
+				},
+			})
+				.setTemplate({
+					template: 'temp0',
+					block0: {
+						componentData: [serviceData[4], serviceData[5]],
+						lg: [0, 4],
+						md: [0, 6],
+					},
+					block1: {
+						componentData: [serviceData[2], serviceData[3]],
+						lg: [3, 3],
+						md: [0, 6],
+					},
+					block2: {
+						componentData: [serviceData[0], serviceData[1]],
+						lg: [2, 4],
+						md: [0, 6],
+					},
 				})
-					.then(function(response){
-						let serviceData = response.data;
-						window.config.setTemplate({
-							template: 'blank',
-							block0: {
-								componentData: [serviceData[0], serviceData[1], serviceData[2], serviceData[3], serviceData[4], serviceData[5]],
-							},
-						})
-							.setTemplate({
-								template: 'temp0',
-								block0: {
-									componentData: [serviceData[4], serviceData[5]],
-									lg: [0, 4],
-									md: [0, 6],
-								},
-								block1: {
-									componentData: [serviceData[2], serviceData[3]],
-									lg: [3, 3],
-									md: [0, 6],
-								},
-								block2: {
-									componentData: [serviceData[0], serviceData[1]],
-									lg: [2, 4],
-									md: [0, 6],
-								},
-							})
-							.setVueConfig(['blank', 'temp0'], ['gridSystem'])
-							.setComponents(serviceData);
-						window.ROOT.info = templates;
-					})
-					.catch(function(error){
-						console.log('Error: ');
-						console.log(error.response);
-					});
-			}
+				.setVueConfig(['blank', 'temp0'], ['gridSystem'])
+				.setComponents(serviceData);
+
+			setROOT();
+		})
+		.catch(function(error){
+			console.log('Error: ');
+			console.log(error.response);
+		});
+})();
+
+
+function setROOT(){
+	window.ROOT = new Vue({
+		el: '#root',
+		components: require('../templates/TemplatesLib.js').default,
+		data: {
+			info: templates,
+			currentTemplate: templates[0],
 		},
-		changeTemp: function(temp){
-			for(let i = 0; i < this.info.length; i++){
-				if(this.info[i].template === temp){
-					this.currentTemplate = this.info[i];
+		computed: {},
+		watch: {},
+		methods: {
+			changeTemp: function(temp){
+				for(let i = 0; i < this.info.length; i++){
+					if(this.info[i].template === temp){
+						this.currentTemplate = this.info[i];
+					}
 				}
 			}
-		}
-	},
-	mounted: function(){
-		this.init();
-	}
-};
-
-window.ROOT = new Vue(rootConfig);
+		},
+		mounted: function(){}
+	});
+}
